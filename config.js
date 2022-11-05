2071fd54163946ac1ae22a1bc57f528f7a2adcf2
Muhhamed Sallam <m.sallam2622000@gmail.com>
2022-11-05 23:54:02 +0200

    adding auth token to socket headers

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "email" : "yosra1@gmail.com",
+    "password" : "123456"
+}
+  
--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const { io } = require("socket.io-client");
 const fs = require("fs");
+const config = require("./config");
+const axios = require('axios');
 
-const socket = io("http://localhost:9000");
 
-socket.on("connect", () => {
-  console.log("connected successfulyl!");
+// Read from config file
+// authenticate using auth API (email, password)
+// Open connection to the server
+// Send EEG data through the socket connection
 
-  const reader = fs.createReadStream(__dirname + "/testo.txt");
+const getToken = async () => {
+  try {
+      const resp = await axios.post('http://localhost:9000/api/v1/auth/login', {
+        email: config.email,
+        password: config.password,
+      });
+      const token = resp.data.data.token;
 
-  reader.on("data", (chunk) => {
-    socket.emit("new-message", chunk);
-  });
-});
+      if(resp.status === 200){
 
-socket.on("disconnect", () => {
-  console.log("disconnected successfully!");
-});
+        const socket = io("http://localhost:9000", {auth: {
+          token: token
+      }});
+
+        socket.on("connect", () => {
+          console.log("connected successfulyl!");
+        
+          const reader = fs.createReadStream(__dirname + "/testo.txt");
+        
+          reader.on("data", (chunk) => {
+            socket.emit("new-message", chunk);
+          });
+        });
+        
+        socket.on("disconnect", () => {
+          console.log("disconnected successfully!");
+        });
+      }
+  } catch (err) {
+      console.error(err);
+  }
+};
+
+
+getToken();
+
