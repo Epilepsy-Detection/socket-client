e7cc7d6080a871fb278cae2e2ae29f22f237c333
Omar Alaa <omaralaaabdelkader@gmail.com>
2022-11-06 19:20:34 +0200

    Refactor code, adjust token, modular approach and verify

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 const { io } = require("socket.io-client");
-const fs = require("fs");
-const config = require("./config");
 const axios = require('axios');
 
-
 // Read from config file
-// authenticate using auth API (email, password)
-// Open connection to the server
-// Send EEG data through the socket connection
+const config = require("./config");
+const readFileAsStream = require("./readFileAsStream");
 
 const authenticateSocket = async () => {
   try {
-      const resp = await axios.post('http://localhost:9000/api/v1/auth/login', {
-        email: config.email,
-        password: config.password,
-      });
-      const token = resp.data.data.token;
+    // authenticate using auth API (email, password)
+    const response = await axios.post(config.API_URL + '/auth/login', {
+      email: config.email,
+      password: config.password,
+    });
+    const token = response.data.data.token;
 
-      if(resp.status === 200){
+    if (response.status !== 200) {
+      throw new Error("Invalid Status code")
+    }
 
-        const socket = io("http://localhost:9000", {auth: {
-          token: token
-      }});
-
-        socket.on("connect", () => {
-          console.log("connected successfulyl!");
-        
-          const reader = fs.createReadStream(__dirname + "/testo.txt");
-        
-          reader.on("data", (chunk) => {
-            socket.emit("new-message", chunk);
-          });
-        });
-        
-        socket.on("disconnect", () => {
-          console.log("disconnected successfully!");
-        });
+    // Open connection to the server
+    const socket = io(config.SOCKET_URL, {
+      auth: {
+        token: `Bearer ${token}`
       }
+    });
+
+    socket.on("connect", () => {
+      console.log("connected successfully!");
+
+      // Send EEG data through the socket connection
+      const fileName = config.FILE_PATH + config.FILE_NAME;
+      readFileAsStream(fileName, (chunk) => {
+        socket.emit("new-message", chunk);
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("disconnected successfully!");
+    });
+
   } catch (err) {
-      console.error(err);
+    console.error(err);
   }
 };
 
