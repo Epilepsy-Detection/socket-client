cf118e0412aa8f595dec4ad296c1a7175b7a602b
omaritocen <omaralaaabdelkader@gmail.com>
2022-11-14 14:22:34 +0200

    Handle error on connection

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { io } = require("socket.io-client");
+const { io } = require('socket.io-client');
 const axios = require('axios');
 
 // Read from config file
-const config = require("./config");
-const readFileAsStream = require("./readFileAsStream");
+const config = require('./config');
+const readFileAsStream = require('./readFileAsStream');
 
 const authenticateSocket = async () => {
   try {
@@ -15,34 +15,36 @@ const authenticateSocket = async () => {
     const token = response.data.data.token;
 
     if (response.status !== 200) {
-      throw new Error("Invalid Status code")
+      throw new Error('Invalid Status code');
     }
 
     // Open connection to the server
     const socket = io(config.SOCKET_URL, {
       auth: {
-        token: `Bearer ${token}`
-      }
+        token: `Bearer ${token}`,
+      },
     });
 
-    socket.on("connect", () => {
-      console.log("connected successfully!");
+    socket.on('connect', () => {
+      console.log('connected successfully!');
 
       // Send EEG data through the socket connection
       const fileName = config.FILE_PATH + config.FILE_NAME;
       readFileAsStream(fileName, (chunk) => {
-        socket.emit("new-message", chunk);
+        socket.emit('new-message', chunk);
       });
     });
 
-    socket.on("disconnect", () => {
-      console.log("disconnected successfully!");
+    socket.on('disconnect', () => {
+      console.log('disconnected successfully!');
     });
 
+    socket.on('connect_error', (err) => {
+      console.log('Failed to connect', err);
+    });
   } catch (err) {
     console.error(err);
   }
 };
 
 authenticateSocket();
-
